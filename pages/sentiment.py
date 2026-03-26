80d7bd3a5bc9aaf279495c78f464b9e5932a5203
Anesu Manjengwa <manjengwap10@gmail.com>
2026-03-26 18:03:36 +0200

    Update sentiment page path and name

--- pages/sentiment.py
+++ pages/sentiment.py
@@ -22,7 +22,7 @@ from data.db import (
 # Import from components
 from components.shared import page_header, kpi_card, status_badge, CHART_LAYOUT
 
-dash.register_page(__name__, path="/page-path", name="Page Name", order=0)
+dash.register_page(__name__, path="/sentiment", name="Customer Sentiment", order=12)
 
 def get_sentiment_data():
     stores = get_stores()
