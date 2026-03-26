3c50e1e2d66d5a27d070aabd4908daeda5b5e20e
Anesu Manjengwa <manjengwap10@gmail.com>
2026-03-26 18:04:17 +0200

    Update page registration for shrinkage page

--- pages/shrinkage.py
+++ pages/shrinkage.py
@@ -22,7 +22,7 @@ from data.db import (
 # Import from components
 from components.shared import page_header, kpi_card, status_badge, CHART_LAYOUT
 
-dash.register_page(__name__, path="/page-path", name="Page Name", order=0)
+dash.register_page(__name__, path="/shrinkage", name="Shrinkage & Loss", order=14)
 
 def layout():
     df = get_shrinkage()
