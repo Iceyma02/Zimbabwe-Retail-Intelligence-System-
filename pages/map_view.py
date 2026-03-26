32f99fd3cf10237c4aabf5bc24de751c2a945224
Anesu Manjengwa <manjengwap10@gmail.com>
2026-03-26 17:59:55 +0200

    Change page registration to Map View with new path

--- pages/map_view.py
+++ pages/map_view.py
@@ -22,7 +22,7 @@ from data.db import (
 # Import from components
 from components.shared import page_header, kpi_card, status_badge, CHART_LAYOUT
 
-dash.register_page(__name__, path="/page-path", name="Page Name", order=0)
+dash.register_page(__name__, path="/map", name="Map View", order=1)
 
 def layout():
     return html.Div([
